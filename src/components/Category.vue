3d97e81a2f30cbccd3fc97b621fcdba2874639a2
shuyanglin <shuyanglin.tw@gmail.com>
2017-03-12 17:49:47 +0800

    first commit

---
+++ src/components/Category.vue
@@ -0,0 +1,34 @@
+ <template>
+    <div class="col-sm-6 col-sm-offset-3">
+      <h1>Get a Free Chuck Norris Quote!</h1>
+      <button class="btn btn-primary" v-on:click="getQuote()">Get a Quote</button>
+      <div class="quote-area" v-if="quote">
+        <h2><blockquote v-html="quote">{{ quote }}</blockquote></h2>      
+      </div>
+    </div>
+  </template>
+
+  <script>
+  import axios from 'axios'
+
+  export default {
+    data () {
+      return {
+        quote: ''
+      }
+    },
+    methods: {
+      getQuote () {
+        axios.get('https://talk.vtaiwan.tw/t/100.json')
+          .then(result => {
+            this.quote = result.data.post_stream.posts[0].cooked
+          })
+      }
+    }
+  }
+  </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
