f28537049c95fdb5e6a21c0b6145002e62e9c8fa
shuyanglin <shuyanglin.tw@gmail.com>
2017-03-18 15:29:31 +0800

    call discourse api

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,9 +1,13 @@
  <template>
     <div class="col-sm-6 col-sm-offset-3">
       <h1>Get a Free Chuck Norris Quote!</h1>
-      <button class="btn btn-primary" v-on:click="getQuote()">Get a Quote</button>
-      <div class="quote-area" v-if="quote">
-        <h2><blockquote v-html="quote">{{ quote }}</blockquote></h2>      
+      <div class="quote-area" v-for="category in categories">
+        <h3>{{ category.name }}</h3>
+        <ul>
+          <li v-for="topic in category.topics">
+            {{ topic.title }}
+          </li>
+        </ul>
       </div>
     </div>
   </template>
@@ -14,20 +18,33 @@
   export default {
     data () {
       return {
-        quote: ''
+        categories: []
       }
     },
+    created () {
+      this.getCategories()
+    },
     methods: {
-      getQuote () {
-        axios.get('https://[hehe]/c/4.json?api_key=[haha]&api_username=[hoho]')
+      getCategories () {
+        axios.get('https://discourse.shuyanglin.com/categories.json?api_key=69152bfd6780a4b8fe9105d1b616ccfc2de28706cbf16a2f0531fb6b08cf38e6&api_username=shu')
           .then(result => {
-            console.log(result)
-            this.quote = result.data.users[0].username
+            this.categories = result.data.category_list.categories
+            var that = this
+            console.log('cat')
+            this.categories.forEach(function (element, index) {
+              that.getTopics(element.id, index)
+              console.log('index:' + index)
+            })
           })
-        /* axios.get('https://talk.vtaiwan.tw/t/100.json')
+      },
+
+      getTopics (id, index) {
+        console.log('top')
+        axios.get('https://discourse.shuyanglin.com/c/' + id + '.json?api_key=69152bfd6780a4b8fe9105d1b616ccfc2de28706cbf16a2f0531fb6b08cf38e6&api_username=shu')
           .then(result => {
-            this.quote = result.data.post_stream.posts[0].cooked
-          }) */
+            this.categories[index].topics = result.data.topic_list.topics
+            this.categories.splice(index, 1, this.categories[index])
+          })
       }
     }
   }
